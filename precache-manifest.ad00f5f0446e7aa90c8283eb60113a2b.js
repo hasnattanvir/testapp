67bb5e33851fed9faccecf702a59e735ce16b3cf
hasnattanvir <hasnattanvir014@gmail.com>
2021-02-16 15:41:46 +0600

    Updates

---
+++ precache-manifest.ad00f5f0446e7aa90c8283eb60113a2b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5c85a47015811cc539515c812a4ee5",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "9a7af47b10972f332151",
+    "url": "/testapp/static/css/2.9bb71e98.chunk.css"
+  },
+  {
+    "revision": "2d24868e3020bccb113e",
+    "url": "/testapp/static/css/main.62b8f7a1.chunk.css"
+  },
+  {
+    "revision": "9a7af47b10972f332151",
+    "url": "/testapp/static/js/2.11e5fb69.chunk.js"
+  },
+  {
+    "revision": "03683fe33f4fbe4656f96802ba8ba754",
+    "url": "/testapp/static/js/2.11e5fb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d24868e3020bccb113e",
+    "url": "/testapp/static/js/main.d4561d38.chunk.js"
+  },
+  {
+    "revision": "517d0011d3db996b5da4",
+    "url": "/testapp/static/js/runtime-main.878ef421.js"
+  },
+  {
+    "revision": "8bf32604ed1f563da77d8694cec46cbe",
+    "url": "/testapp/static/media/bannerhome.8bf32604.jpg"
+  },
+  {
+    "revision": "33193a2a2ca983377751b149f4eb1918",
+    "url": "/testapp/static/media/serphoto1.33193a2a.jpg"
+  },
+  {
+    "revision": "771fcbd967815e079bb733f5dceb635d",
+    "url": "/testapp/static/media/serphoto2.771fcbd9.png"
+  },
+  {
+    "revision": "aa93b4efa80cb7082938fd68d8373418",
+    "url": "/testapp/static/media/serphoto3.aa93b4ef.jpg"
+  },
+  {
+    "revision": "19c8ba47e1f8b857fe9c4352bde36239",
+    "url": "/testapp/static/media/serphoto4.19c8ba47.jpg"
+  },
+  {
+    "revision": "7b217f49c6c723a3518cb88502d24840",
+    "url": "/testapp/static/media/serphoto5.7b217f49.jpg"
+  },
+  {
+    "revision": "7171c2b79303007fe664e7c4bf6931cb",
+    "url": "/testapp/static/media/serphoto6.7171c2b7.jpg"
+  },
+  {
+    "revision": "e71b60eb7cca58c696faccddeb6bad03",
+    "url": "/testapp/static/media/tanvir.e71b60eb.png"
+  },
+  {
+    "revision": "f030ed60c3033cbb5c5b12b5deea0740",
+    "url": "/testapp/static/media/unnamed.f030ed60.png"
+  }
+]);
